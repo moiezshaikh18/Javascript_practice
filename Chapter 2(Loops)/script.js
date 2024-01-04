f5a170c2e60f16e6d95f86b341ba95035a610b6a
mueej <v-mueej@fieldera.com>
2024-01-04 19:44:37 +0530

    chapter 2 revison done

---
+++ Chapter 2(Loops)/script.js
@@ -0,0 +1,87 @@
+// For Loop
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log("This is For Loop", i);
+// }
+
+// let dishes = ["biryani", "dalcha", "pulab"];
+
+// for (let j = 0; j < dishes.length; j++) {
+//   console.log(dishes[j]);
+// }
+
+// for (let k = dishes.length - 1; k >= 0; k--) {
+//   console.log(dishes[k]);
+// }
+
+// // iteration and value of i
+
+// // while Loop
+
+// let f = 0;
+// while (f < dishes.length - 1) {
+//   console.log("thsos while loop", f);
+//   f++;
+// }
+
+// if, else,else if statement
+
+// let budget = 4600;
+
+// if (budget > 6000) {
+//   console.log("will do it dinner");
+// } else if (budget > 4500) {
+//   console.log("dinner at cheap place");
+// } else {
+//   console.log("not possib;e");
+// }
+
+// Break and Continue Statement
+
+// for (let i = 0; i <= 10; i++) {
+//   if (i == 2) {
+//     break;
+//   }
+//   console.log("This is For Loop", i);
+// }
+
+// Logical Operator
+
+//&& , || , !
+
+// let password = "qwerty@";
+
+// if (password.length > 5 && password.includes("@")) {
+//   console.log("Its strong");
+// } else {
+//   console.log("Change yor passwod");
+// }
+
+// Variable and Block Scopes
+
+// let score = 90; // Global Scope
+
+// if (true) {
+//   let score = 50;
+//   console.log(score);
+// }
+
+// ternary operator
+
+// let age = 30;
+
+// let result = age > 18 ? "drive" : "not allowed";
+// console.log("result", result);
+
+// Switch Statement
+
+// let age = 16;
+
+// switch (age) {
+//   case 16:
+//     console.log(" allowed");
+//     break;
+//   default:
+//     console.log(" not allowed");
+//     break;
+// }
