53fdaa695820537a4ddca73c38ee8a686957a8f3
mueej <v-mueej@fieldera.com>
2024-01-05 14:49:25 +0530

    Practice set Chapter 2 done

---
+++ Chapter 2(Loops)/practiceTwo.js
@@ -0,0 +1,68 @@
+// Chapter 2 (control Flow and Conditinal')
+//2.1
+
+// let i = 4;                              Output:- 4 0 ,5 0, 6 0
+// for (let j = 0; i < 10; i++) {
+//   if (j === 1 || i === 6) {
+//     continue;
+//   } else {
+//     console.log(i, j);
+
+//     if (i === 7) {
+//       break;
+//     }
+//   }
+// }
+
+// 2.2
+
+// let i = 0;                    0 to 4
+
+// for (i; i < 5; i++) {
+//   console.log(i);
+// }
+
+//2.3
+
+// for (let i = 10; i > -11; i--) {
+//   console.log("Number", i < 0 ? i * -1 : i);
+// }
+
+//2.4
+
+// let country, english;
+// let food = ["spanish", "english"];
+// if (country && english) {
+//   console.log("Has 10 million people speak");
+// } else if (food.includes("spanish")) {
+//   console.log("spanish food");
+// } else if (food.includes("english")) {
+//   console.log("english food");
+// }
+
+//2.5
+
+// for (let i = 0; i < 10; i++) {
+//   //output:-0 - 9
+//   console.log(i);
+// }
+
+// console.log("sds", i);
+
+//2.6
+// let age = 20;
+// let height = 5.11;
+// if (age > 18) {
+//   if (height > 5.1) {
+//     console.log("Perfect for drive");
+//   }
+// }
+
+//2.7
+
+// let grade = "A";
+// let pasingYear = 2018;
+
+// let result = grade == "A" && pasingYear < 2020 ? "Qualify" : "no";
+
+// console.log("result", result);
