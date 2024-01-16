57aeacfa0d2e07e7615ece24debb577631bde2ce
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-01-16 23:22:25 +0530

    solve 7.1 que

--- Chapter 7(Array Methods)/)/practiceSeven.js
+++ Chapter 7(Array Methods)/)/practiceSeven.js
@@ -1,1 +1,7 @@
 console.log("Tommorow Ill start the chapter & practice");
+
+//7.1
+
+let admins = ["john", "paul", "Neha", "harry"];
+
+console.log(admins.slice(2)); // start index consider but end index excluding
