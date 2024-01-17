4a4f96cec494e8acc6ff858f5a70446c277bb720
mueej <v-mueej@fieldera.com>
2024-01-17 18:28:31 +0530

    Chapter 9 Done

---
+++ Chapter 9(Local Storage)/script.js
@@ -0,0 +1,52 @@
+// Local Storage
+
+// localStorage.setItem("passion", "coder");
+// localStorage.setItem("age", 24);
+
+// Get local storage
+
+// let getIt = localStorage.getItem("age");
+
+// console.log(localStorage);
+// console.log(getIt);
+
+//Upadete
+
+// console.log(localStorage.setItem("age", 26));
+
+//Remove Item
+
+// console.log(localStorage.removeItem("passion"));
+// console.log(localStorage);
+
+//Complex data
+
+// let vehicles = [
+//   {
+//     company: "Honda",
+//     model: "2009",
+//   },
+//   {
+//     company: "Toyota",
+//     model: "2010",
+//   },
+// ];
+
+// let strCon = JSON.stringify(vehicles);
+
+// console.log(localStorage.setItem("vehicles", vehicles));
+
+// let conStr = JSON.parse(localStorage.getItem("vehicles"));
+
+// console.log(conStr);
+
+//////////////////////////////////////////////////
+
+//Practice
+
+// var myHobby = "cricket";
+// var age = 15;
+
+// localStorage.setItem("myHobby,age", myHobby, age);
+
+// console.log(localStorage);
