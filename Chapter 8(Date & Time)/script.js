1cb2cb270382f378a3b3c2517b945cb82f03d32f
mueej <v-mueej@fieldera.com>
2024-01-17 15:27:29 +0530

    Chapter 8 Done

---
+++ Chapter 8(Date & Time)/script.js
@@ -0,0 +1,48 @@
+// Date and Time
+
+//using the date methods
+
+// let date = new Date(0);
+
+// console.log("date", date);
+
+// to get the time
+
+// let currentDate = new Date();
+
+// console.log(currentDate.getTime());
+
+// Complete date and time
+
+//Year
+// console.log(currentDate.getFullYear());
+
+// Month
+// console.log(currentDate.getMonth());
+
+//daay
+// console.log(currentDate.getDay());
+
+// Hours
+
+// console.log(currentDate.getDay());
+
+////////////////////////////////// Digital Clock ///////////////////////////
+
+// let hours = document.querySelector(".hours");
+// let min = document.querySelector(".min");
+// let sec = document.querySelector(".sec");
+
+// let ticking = function () {
+//   let currentDate = new Date();
+
+//   let getHours = currentDate.getHours();
+//   let getMin = currentDate.getMinutes();
+//   let getSec = currentDate.getSeconds();
+
+//   hours.innerHTML = getHours;
+//   min.innerHTML = getMin;
+//   sec.innerHTML = getSec;
+// };
+
+// setInterval(ticking, 1000);
